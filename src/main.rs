d2adc4f900380df6feede60a5a6738e144e21bef
rizan21 <rizanfarooqui@gmail.com>
2022-10-28 17:24:45 +0530

    cargo run will now get the bootimage and run it on qemu changed the helo text to pokemon :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 #![no_std]
 #![no_main]
 
@@ -8,16 +9,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+static HELLO: &[u8] = b"POKEMON IS A GOOD GAME!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
+
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+
     loop {}
 }
