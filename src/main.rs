1d7d451ef98b2938ce49c668b55b6904f339c9ab
rizan21 <rizanfarooqui@gmail.com>
2022-11-20 11:05:35 +0530

    Added macros for print! and println! use a Mutex to avoid race conditions OS can print bytes and strings

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,13 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("ok_{}", info);   
     loop {}
 }
 
-static HELLO: &[u8] = b"POKEMON IS A GOOD GAME!";
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+pub extern "C" fn _start() {
+    println!("Hello World! {}", "POKEMON IS A GOOD GAME!");
     loop {}
 }
