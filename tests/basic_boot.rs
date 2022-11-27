8f2342ae867d23c54e81b6c0b8dc8d9d1138f5c6
rizan21 <rizanfarooqui@gmail.com>
2022-11-27 22:56:27 +0530

    made tests to check basic functions and a separate executable for them

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -2,30 +2,30 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rusty_af_os::test_runner)]
+// #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use rusty_af_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rusty_af_os::test_panic_handler(info)
 }
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
